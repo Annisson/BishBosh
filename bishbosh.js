ed5e5b6789babbdcf39a454365e1ff0175b2cdac
Annica Andersson <annicaandersson@live.se>
2024-04-08 02:46:56 +0200

    Add validation check

--- bishbosh.js
+++ bishbosh.js
@@ -4,18 +4,21 @@ function UserInput()
     const boshInput = document.getElementById('formBoshInput').value;
     const loopInput = document.getElementById('formLoopInput').value;
 
-    ValidateUserInput(bishInput, boshInput, loopInput); // Call the function to do the validation check
+    const validInput = ValidateUserInput(bishInput, boshInput, loopInput); // Call the function to do a validation check
 
-
-    BishBosh(bish, bosh, loop); // Call the function that will print the result
-
-
+    if (validInput) { // If validation is successful, parse input to int and call for the function to print the numbers/words
+        const bish = parseInt(bishInput);
+        const bosh = parseInt(boshInput);
+        const loop = parseInt(loopInput);
+        BishBosh(bish, bosh, loop);
+    }
 }
 
 function ValidateUserInput(bishInput, boshInput, loopInput) // Validate user input, add checks for number and negative numbers
 {
-    if (!bishInput || !boshInput || !loopInput) {
-        
+    if (!bishInput || isNaN(bishInput) || bishInput <= 0 || // Checks if a field is not filled out, NaN(not a numer), or is a negative number
+    !boshInput || isNaN(boshInput) || boshInput <= 0 ||
+    !loopInput || isNaN(loopInput) || loopInput <= 0) {
         return false;
     }
 
