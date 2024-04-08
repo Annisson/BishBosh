7b56d44f1bbef678a4b719e4535486d4f9fda75c
Annica Andersson <annicaandersson@live.se>
2024-04-08 02:42:48 +0200

    Add WIP for functions

--- bishbosh.js
+++ bishbosh.js
@@ -1,5 +1,50 @@
+function UserInput()
+{
+    const bishInput = document.getElementById('formBishInput').value; // Save the user input
+    const boshInput = document.getElementById('formBoshInput').value;
+    const loopInput = document.getElementById('formLoopInput').value;
+
+    ValidateUserInput(bishInput, boshInput, loopInput); // Call the function to do the validation check
 
 
+    BishBosh(bish, bosh, loop); // Call the function that will print the result
 
 
+}
 
+function ValidateUserInput(bishInput, boshInput, loopInput) // Validate user input, add checks for number and negative numbers
+{
+    if (!bishInput || !boshInput || !loopInput) {
+        
+        return false;
+    }
+
+    return true;
+
+}
+
+function BishBosh(bish, bosh, loop)   // Add the function here for printing the result
+{
+
+   for (let i = 1; i <= loop; i++) {
+        if(i % bish === 0 && i % bosh === 0)
+        {
+            console.log("Bish-Bosh");
+        }
+        else if(i % bish === 0)
+        {
+            console.log("Bish");
+        }
+        else if(i % bosh === 0)
+        {
+            console.log("Bosh");
+        }
+        else
+        {
+            console.log(i);
+        }
+
+
+    } 
+    
+}
